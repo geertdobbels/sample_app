72a28f71d09ee85ed672cec46cfeb1c4cc7231ce
geertdobbels <dobbels.geert@gmail.com>
2014-06-09 11:04:37 +0200

    now with bundle exec rake assets:precompile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   	@user = User.new
   end
   def index
-  	@users = User.paginate(page: params[:page])
+  	@users = User.paginate(page: params[:page], per_page: 10)
   end
   def show
 	 @user = User.find(params[:id])
